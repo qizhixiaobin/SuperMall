26268b7d9843fdd12b62908e276939efe3ce5431
abin <abin_fu@foxmail.com>
2024-07-04 14:20:07 +0800

    首次创建maven项目

---
+++ src/main/java/com/abin/utils/JsonResponse.java
@@ -0,0 +1,11 @@
+/*
+ * @Author: abin abin_fu@foxmail.com
+ * @Date: 2024-07-04 11:52:39
+ * @LastEditors: abin abin_fu@foxmail.com
+ * @LastEditTime: 2024-07-04 11:53:01
+ */
+package com.abin.utils;
+
+public class JsonResponse {
+
+}
